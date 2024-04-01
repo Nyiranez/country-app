1a2100c015759591f057b518870dcb5ef1dec96a
Nyiranez <nyiranezalouise93@gmail.com>
2024-04-01 12:33:51 +0200

    my added contact file

---
+++ src/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Contact = () => {
+  return (
+    <div>
+        <h1 className='text-red-400 font-bold text-4xl'>Welcome To Contact Page</h1>
+    </div>
+  )
+}
