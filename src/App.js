a8bef39adfd8ed5f79200549f05f846a9cf45b4b
Nyiranez <nyiranezalouise93@gmail.com>
2024-04-01 11:47:56 +0200

    my country app commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Footer } from './components/footer';
+import { Header } from './components/header';
+import{HeadB} from './components/pages/home'
+import {Routes,Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='pl-2 pr-2'>
+      <div className='container'>
+      <Header />
+       <Routes>
+       <Route path="/" element={<HeadB />} />
+       </Routes>
+      </div>
+     
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+        <div className=" bg-slate-50 h-24 w-full flex dm:flex-row flex-col md:justify-around items-center">
+        <h1 className="text-4xl text-blue-600 font-bold">Logoipsum</h1>
+        <p className='text-gray-600 mt-4'>Copyright © 2024. All rights reserved.</p>
+        </div>
+    </div>
+  )
+}
