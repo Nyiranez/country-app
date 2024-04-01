faab03f00ea0246e2a882883437707797e026950
Nyiranez <nyiranezalouise93@gmail.com>
2024-04-01 12:32:24 +0200

    content in about page

---
+++ src/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <h1 className='text-red-400 font-bold text-4xl'>Welcome To About Page</h1>
+    </div>
+  )
+}
