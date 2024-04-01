38658b225beb90fb73fda524e2c98989ef7e35a4
Nyiranez <nyiranezalouise93@gmail.com>
2024-04-01 12:34:45 +0200

    home is added

---
+++ src/home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+        <h1 className='text-red-400 font-bold text-4xl'>Welcome To Home Page</h1>
+    </div>
+  )
+}
