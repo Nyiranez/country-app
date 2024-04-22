ea2a2970db98f344531828c5bd7798b62f4762d6
Nyiranez <nyiranezalouise93@gmail.com>
2024-04-22 22:46:39 +0200

    a anchor tage deleted

--- src/components/pages/pagination.js
+++ src/components/pages/pagination.js
@@ -4,12 +4,12 @@ export const Pagination = () => {
   return (
     <div>
         <div className='space-x-4 flex  justify-center mt-8 mb-4'>
-        <button className='bg-slate-50 py-1 px-2 border border-gray-300'><a href="">1</a></button>
-        <button className='bg-slate-50 py-1 px-2 border border-gray-300'><a href="">2</a></button>
-        <button className='bg-slate-50 py-1 px-2 border border-gray-300'><a href="">3</a></button>
-        <button className='bg-slate-50 py-1 px-2 border border-gray-300'><a href="">4</a></button>
-        <button className='bg-slate-50 py-1 px-2 border border-gray-300'><a href="">5</a></button>
-        <button className='bg-slate-50 py-1 px-2 border border-gray-300'><a href="">></a></button>
+        <button className='bg-slate-50 py-1 px-2 border border-gray-300'>1</button>
+        <button className='bg-slate-50 py-1 px-2 border border-gray-300'>2</button>
+        <button className='bg-slate-50 py-1 px-2 border border-gray-300'>3</button>
+        <button className='bg-slate-50 py-1 px-2 border border-gray-300'>4</button>
+        <button className='bg-slate-50 py-1 px-2 border border-gray-300'>5</button>
+       
         </div>
       
     </div>
